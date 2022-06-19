7b802077fb42f752f6078e5a042752fd32a9589a
Felipe Passos <felipe.carvalho.passos@gmail.com>
2022-06-19 14:19:50 -0300

    File transfer

--- main.py
+++ main.py
@@ -1,35 +1,58 @@
 import socket
+import tqdm
+import os
 
-HOST = '127.0.0.1'      # Endereco IP do Servidor
-PORT = 5000             # Porta que o Servidor está
+# device's IP address
+SERVER_HOST = "127.0.0.1"
+SERVER_PORT = 5001
+# receive 4096 bytes each time
+BUFFER_SIZE = 4096
+SEPARATOR = "<SEPARATOR>"
 
-tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-origem = (HOST, PORT)
+# create the server socket
+# TCP socket
+s = socket.socket()
 
-# Colocando um endereço IP e uma porta no Socket
-tcp.bind(origem)
+# bind the socket to our local address
+s.bind((SERVER_HOST, SERVER_PORT))
 
-# Colocando o Socket em modo passivo
-tcp.listen(1)
+# enabling our server to accept connections
+# 5 here is the number of unaccepted connections that
+# the system will allow before refusing new connections
+s.listen(5)
+print(f"[*] Listening as {SERVER_HOST}:{SERVER_PORT}")
 
-print('\nServidor TCP iniciado no IP', HOST, 'na porta', PORT)
+# accept connection if there is any
+client_socket, address = s.accept() 
+# if below code is executed, that means the sender is connected
+print(f"[+] {address} is connected.")
 
-while True:
-    # Aceitando uma nova conexão
-    conexao, cliente = tcp.accept()
-    print('\nConexão realizada por:', cliente)
+# receive the file infos
+# receive using client socket, not server socket
+received = client_socket.recv(BUFFER_SIZE).decode()
+filename, filesize = received.split(SEPARATOR)
+# remove absolute path if there is
+filename = os.path.basename(filename)
+# convert to integer
+filesize = int(filesize)
 
+# start receiving the file from the socket
+# and writing to the file stream
+progress = tqdm.tqdm(range(filesize), f"Receiving {filename}", unit="B", unit_scale=True, unit_divisor=1024)
+with open(filename, "wb") as f:
     while True:
-        # Recebendo as mensagens através da conexão
-        mensagem = conexao.recv(1024)
-        if not mensagem:
+        # read 1024 bytes from the socket (receive)
+        bytes_read = client_socket.recv(BUFFER_SIZE)
+        if not bytes_read:    
+            # nothing is received
+            # file transmitting is done
             break
+        # write to the file the bytes we just received
+        f.write(bytes_read)
+        # update the progress bar
+        progress.update(len(bytes_read))
 
-        # Exibindo a mensagem recebida
-        print('\nCliente..:', cliente)
-        print('Mensagem.:', mensagem.decode())
-
-    print('Finalizando conexão do cliente', cliente)
-
-    # Fechando a conexão com o Socket
-    conexao.close()
+# close the client socket
+client_socket.close()
+# close the server socket
+s.close()
